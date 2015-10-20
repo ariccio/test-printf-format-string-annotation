06e90bff4e32f608033bd5560f440d638a780f6f
Alexander Riccio <alexander@riccio.com>
2015-10-20 00:45:52 -0400

    vs2015

--- minimal_test-printf_annotation/test-printf_annotation/test-printf-annotation.cpp
+++ minimal_test-printf_annotation/test-printf_annotation/test-printf-annotation.cpp
@@ -18,6 +18,20 @@ void boring_printf_4( _In_z_ const char* const ptr ) {
 	printf( "%s", ptr );
 	}
 
+void boring_vdebugf( _In_z_ _Printf_format_string_ const char* const ptr, va_list args ) {
+	const rsize_t buffer_size = 512u;
+	char buffer[ buffer_size ] = { };
+	_vsnprintf_s( buffer, buffer_size, _TRUNCATE, ptr, args );
+	OutputDebugStringA( buffer );
+	}
+
+void boring_debugf( _In_z_ _Printf_format_string_ const char* const ptr, ... ) {
+	va_list args;
+	va_start( args, ptr );
+	boring_vdebugf( ptr, args );
+	va_end( args );
+	}
+
 int main( ) {
 	char three_char_str_format[ 30 ];
 	three_char_str_format[ 0 ] = '%';
@@ -32,6 +46,8 @@ int main( ) {
 
 	boring_printf_4( three_char_str_format );
 
+	boring_debugf( three_char_str_format, "yo" );
+
 	three_char_str_format[ 2 ] = '`';
 
 	boring_printf_1( three_char_str_format );
@@ -41,4 +57,8 @@ int main( ) {
 	boring_printf_3( three_char_str_format );
 
 	boring_printf_4( three_char_str_format );
+
+	boring_debugf( three_char_str_format, "yo" );
+
+
 }
