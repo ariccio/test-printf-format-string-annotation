533adb7dcfa38d8a2488adc09d3b34a0e340baf8
Alexander Riccio <alexander@riccio.com>
2015-10-20 01:11:42 -0400

    minimized

--- minimal_test-printf_annotation/test-printf_annotation/test-printf-annotation.cpp
+++ minimal_test-printf_annotation/test-printf_annotation/test-printf-annotation.cpp
@@ -1,28 +1,19 @@
 #include <cstdio>
 #include <windows.h>
 
-void boring_printf_3( _In_z_ _Printf_format_string_ const char* const ptr ) {
-	printf( ptr, "" );
-	}
-
 void boring_printf_4( _In_z_ const char* const ptr ) {
-	printf( "%s", ptr );
+	printf( "%s\r\n", ptr );
 	}
 
 int main( ) {
-	char three_char_str_format[ 30 ];
+	char three_char_str_format[ 3 ];
 	three_char_str_format[ 0 ] = '%';
 	three_char_str_format[ 1 ] = 's';
 	three_char_str_format[ 2 ] = 0;
-
-	boring_printf_3( three_char_str_format );
 
 	boring_printf_4( three_char_str_format );
 
 	three_char_str_format[ 2 ] = '`';
 
-	boring_printf_3( three_char_str_format );
-
 	boring_printf_4( three_char_str_format );
-
 }
